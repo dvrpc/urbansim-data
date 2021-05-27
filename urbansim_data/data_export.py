3f408f64cc2f4919a6ca654039a3c0702abcee0b
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-05-27 14:26:54 -0400

    update import & function call

--- urbansim_data/data_export.py
+++ urbansim_data/data_export.py
@@ -3,7 +3,7 @@ from .db import _db, GDRIVE_FOLDER
 from pg_data_etl import Database
 
 
-def export_shp(tablename: str, output_path: Path = GDRIVE_FOLDER, db: Database = _db):
+def export_shp(tablename: str, output_path: Path = GDRIVE_FOLDER, db: Database = _db) -> None:
     """
     Export a spatial table from SQL to a shapefile
     """
@@ -13,4 +13,4 @@ def export_shp(tablename: str, output_path: Path = GDRIVE_FOLDER, db: Database =
     print("-" * 80)
     print(f"Exporting {tablename} to {shp_output_path}")
 
-    db.ogr2ogr_export(tablename, shp_output_path)
+    db.export_gis(table_or_sql=tablename, filepath=shp_output_path)
