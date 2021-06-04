8e703691f3d406c887cb55a88529a2e77f2d3c53
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-06-04 17:19:08 -0400

    add help text

--- urbansim_data/cli.py
+++ urbansim_data/cli.py
@@ -7,7 +7,9 @@ from urbansim_data.data_export import export_shp as _export_shp
 
 @click.group()
 def main():
-    """'urbansim' provides command-line access to the Python/SQL tool"""
+    """'urbansim' provides command-line access to
+    functions that import, manipulate, and export
+    development data in helpful formats."""
     pass
 
 
@@ -18,10 +20,26 @@ def import_data():
 
 
 @click.command()
-@click.option("-pr", "--project-table", default=_find_latest_project_table())
-@click.option("-pa", "--parcel-table", default="parcels")
-@click.option("-y", "--year-filter", default="start_year >= 2020")
+@click.option(
+    "-pr",
+    "--project-table",
+    default=_find_latest_project_table(),
+    help="Name of the project table. Defaults to most recent download.",
+)
+@click.option(
+    "-pa",
+    "--parcel-table",
+    default="parcels",
+    help="Name of the parcel geo-table. Defaults to 'parcels'",
+)
+@click.option(
+    "-y",
+    "--year-filter",
+    default="start_year >= 2020",
+    help="SQL-valid filter to limit developments to a certain time range. Defaults to 'start_year >= 2020'",
+)
 def assign_geom_to_projects(project_table, parcel_table, year_filter):
+    """Transform development table into spatial data, using parcel centroid"""
     projects_to_parcels(
         project_table=project_table, parcel_table=parcel_table, year_filter=year_filter
     )
